abeb9685146430837ee238eceee9e294bae94367
Amanda Vieira <108702135+amandav777@users.noreply.github.com>
2024-02-07 20:34:13 -0300

    Exercício 1

---
+++ Básico/1.cs
@@ -0,0 +1,18 @@
+// batismo em c#
+Console.WriteLine("Hello, World!");
+
+// CONCEITOS
+// (Console.ReadLine()) = input
+// (Console.WriteLine()) = print
+// double = float
+// tipar antes
+
+
+// Criar exercício para descobrir a área de um retângulo
+
+// parse converte, igual o js
+float a = float.Parse(Console.ReadLine());
+float b = float.Parse(Console.ReadLine());
+
+float areaRetangulo = a * b;
+Console.WriteLine(areaRetangulo);
