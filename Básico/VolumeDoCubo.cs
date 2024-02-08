63c22c14f1ff36ef05a388ffd1fbaed043ebec08
Amanda Vieira <108702135+amandav777@users.noreply.github.com>
2024-02-07 22:26:41 -0300

    aaaaaaaaaaaaaaa

---
+++ Básico/VolumeDoCubo.cs
@@ -0,0 +1,7 @@
+        Console.WriteLine("Insira o lado do cubo:");
+        float ladoA = float.Parse(Console.ReadLine());
+
+
+        float volume = ladoA * ladoA * ladoA;
+
+        Console.WriteLine($"O volume do cubo é {volume}.");
---
+++ Básico/ÁreaDeCircunferencia.cs
@@ -0,0 +1,6 @@
+ Console.WriteLine("Insira o raio do círculo:");
+        float raio = float.Parse(Console.ReadLine());
+
+        float area = Math.PI * raio * raio;
+
+        Console.WriteLine($"A área do círculo é {area}.");
