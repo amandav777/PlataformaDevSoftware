04088dc57dcd1760562f341ac91c406d5a11e181
Amanda Vieira <108702135+amandav777@users.noreply.github.com>
2024-02-07 22:07:44 -0300

    feat: atualizacao ex 1

--- Básico/1.cs
+++ Básico/1.cs
@@ -11,8 +11,12 @@ Console.WriteLine("Hello, World!");
 // Criar exercício para descobrir a área de um retângulo
 
 // parse converte, igual o js
-float a = float.Parse(Console.ReadLine());
+Console.WriteLine("Insira a altura do retângulo:");
+float a =  float.Parse(Console.ReadLine());
+Console.WriteLine("Insira a base do retângulo:");
 float b = float.Parse(Console.ReadLine());
 
 float areaRetangulo = a * b;
-Console.WriteLine(areaRetangulo);
+Console.WriteLine($"A área do retângulo é {areaRetangulo}.");
+
+
